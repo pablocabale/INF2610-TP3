496d898c09233994627bc501f18bda3eefd67132
cabaleguerra <pablo.cabale@polymtl.ca>
2024-11-26 16:56:00 -0500

    1.6

--- TP3_Groupe3/Section 1/deadlock_6.c
+++ TP3_Groupe3/Section 1/deadlock_6.c
@@ -1,3 +1,19 @@
+/*
+Exclusion mutuelle: Il n'y a pas de ressources exclusives dans ce code. Juste la valeur de flag (ligne 30) est partagée entre les threads.
+cependant elle n'est pas modifiée par les threads, donc il n'y a pas de problème d'exclusion mutuelle.
+
+Detention et attente: Il n'y a pas de détention et attente dans ce code. Les threads ne détiennent pas de ressources et n'attendent pas d'autres ressources.
+
+Pas de réquisition: Les threads ne réquisitionnent pas de ressources dans ce code.
+
+Attente circulaire: Il n'y a pas d'attente circulaire dans ce code.
+
+Correction de l'interblocage: les conditions d'interblocage de Coffman ne sont pas satisfaites dans ce code.
+cependant, le programme sera toujours exécuté indéfiniment car la variable flag n'est jamais modifiée.
+car rand() % 10 == 10 n'est jamais vrai (ligne 37). Pour eviter des boucles infinies, il faudrais que la 
+fonction changeFlag() (ligne 36) modifie puisse toujours modifier la valeur de flag a 1.
+*/
+
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
@@ -11,14 +27,14 @@ int flag = 0;
 
 void* function_that_does_something(void* arg) {
     while (1) {
-        if (flag)
+        if (flag) // flag est partagé entre les threads mais n'est pas modifié
             break;
     }
     pthread_exit(NULL);
 }
 
 void changeFlag(){
-    if (rand() % 10 == 10)
+    if (rand() % 10 == 10) // Toujours faux
         flag = 1;
     printf("Signal called\n");
 }
